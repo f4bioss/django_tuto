3dc7f32371853893696c4c453d019d07bb22356f
Fabio Santana <f4bioss@gmail.com>
2015-12-23 14:57:40 -0200

    Add support Python 2

--- polls/models.py
+++ polls/models.py
@@ -1,11 +1,11 @@
+from __future__ import unicode_literals
 import datetime
-
-#from __future__ import unicode_literals
 
 from django.db import models
 from django.utils import timezone
 from django.utils.encoding import python_2_unicode_compatible
 
+@python_2_unicode_compatible  # only if you need to support Python 2
 class Question(models.Model):
     question_text = models.CharField(max_length=200)    
     pub_date = models.DateTimeField('date published')
@@ -13,6 +13,7 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+@python_2_unicode_compatible  # only if you need to support Python 2
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
